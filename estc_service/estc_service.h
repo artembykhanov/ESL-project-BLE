c545c68de7e72609f1e947434e4db6c609093a6e
Artem Bykhanov <rigli.fx@gmail.com>
2025-03-07 02:17:10 +0000

    Workshop 10: Create BLE Service. Closes #2

--- estc_service/estc_service.h
+++ estc_service/estc_service.h
@@ -1,33 +1,3 @@
-/**
- * Copyright 2022 Evgeniy Morozov
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
- * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE
-*/
-
 #ifndef ESTC_SERVICE_H__
 #define ESTC_SERVICE_H__
 
@@ -36,15 +6,17 @@
 #include "ble.h"
 #include "sdk_errors.h"
 
-// TODO: 1. Generate random BLE UUID (Version 4 UUID) and define it in the following format:
-// #define ESTC_BASE_UUID { 0xF6, 0xCE, 0x0F, 0xC4, 0xCE, 0x9F, /* - */ 0xC3, 0x99, /* - */ 0xF7, 0x4D, /* - */ 0xDB, 0xB9, /* - */ 0x00, 0x00, 0xEC, 0x39 } // UUID: EC39xxxx-B9DB-4DF7-99C3-9FCEC40FCEF6
+// DONE: 1. Generate random BLE UUID (Version 4 UUID) and define it in the following format:
+#define RANDOM_BASE_UUID {0x41, 0xC1, 0xB0, 0x2D, 0x56, 0x32, /* - */ 0x1B, 0xA2, /* - */ 0xF9, 0x4A, /* - */ 0x3A, 0x6B, /* - */ 0xF1, 0xB1, 0x69, 0xA9}
+// UUID: a969b1f1-6b3a-4af9-a21b-32562db0c141
 
-// TODO: 2. Pick a random service 16-bit UUID and define it:
-// #define ESTC_SERVICE_UUID 0xabcd
+// DONE: 2. Pick a random service 16-bit UUID and define it:
+#define RANDOM_SERVICE_UUID 0x180D
 
 typedef struct
 {
     uint16_t service_handle;
+    uint8_t uuid_type;
 } ble_estc_service_t;
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service);
